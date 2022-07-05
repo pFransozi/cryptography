9d2ba6aca9e0c98ba9d213a78d42c68d70e4f3e4
Philipe Fransozi <philipehf@gmail.com>
2022-07-05 12:32:00 -0300

    added new files.

--- README.md
+++ README.md
@@ -3,6 +3,26 @@ In this repository, I gather my studies on cryptography, theory and practice.
 I agree with the statement that cryptography is the primary tool for protecting information and, in cryptography, encryption is a cornerstone of data confidentiality, authentication and integrity. But typically the effectiveness of cryptography depends on context, which means in each context the cryptography method or configuration must be adapted for guarantee one or more of the CIA principle.
 
 
-further read:
+references and further reading:
 
-https://cryptography.io/en/latest/
+[1] Kahn, David. The Codebreakers: The Story of Secret Writing, 1967.
+[2] https://cryptography.io/en/latest/.
+[3] W. Diffie and M. Hellman, “New directions in cryptography.” IEEE
+Trans. Information Theory. 22, 6 (September 2006), 644–654. DOI:
+https://doi.org/10.1109/TIT.1976.1055638.
+[6] Bruce Schneier and Phil Sutherland, Applied Cryptography:
+Protocols, Algorithms, and Source Code in C (Second Edition),
+ISBN: 978-0-471-12845-8. John Wiley & Sons, Inc., USA. 1995.
+William Stallings, Cryptography and Network Security: Principles
+and Practice. Upper Saddle River, N.J: Prentice Hall, 1999.
+[9] Neal Koblitz, A Course in Number Theory and Cryptography.
+New York: Springer-­Verlag, 1994.
+[10] Neal Koblitz and A J. Menezes, Algebraic Aspects of
+Cryptography, 1999.
+[11] Oded Goldreich, Foundations of Cryptography: Basic Tools.
+Cambridge: Cambridge University Press, 2001. Print.
+[12] Oded Goldreich, Modern Cryptography, Probabilistic Proofs and
+Pseudorandomness. Berlin: Springer, 1999. Print.
+[14] Bruce Schneier, Secrets and Lies: Digital Security in a Networked
+World. New York: John Wiley, 2000.
+[19] Namespace System.Security.Cryptography, ­ https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography?view=net-6.0
---
+++ code/c#/aes_using_aesmanaged/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace AESExampleOfImplementation
+{
+    class AESExampleOfImplementation
+    {
+        public static void Main()
+        {
+            string genuineMessage = "Welome to Apress!";
+
+            using (AesManaged encryptor_aes = new AesManaged())
+            {
+                byte[] encrypted = EncryptStringToBytes_Aes(
+                                original,
+                                encryptor_aes.Key,
+                                encryptor_aes.IV
+                        );
+
+                string roundtrip = DecryptStringFromBytes_Aes(
+                                encrypted,
+                                encryptor_aes.Key,
+                                encryptor_aes.IV
+                        );
+
+                Console.WriteLine("Original:   {0}", genuineMessage);
+                Console.WriteLine("Round Trip: {0}", roundtrip);
+
+                Console.WriteLine("The encrypted message is (byte-to-byte): {0}",
+                                    PrintByteArray(genuineMessage)
+                );
+
+                Console.WriteLine("The encrypted message is (default view): {0}",
+                                    Encoding.Default.GetString(genuineMessage)
+                );
+
+                Console.WriteLine("The encrypted message is (UTF8 view): {0}",
+                                    Encoding.UTF8.GetString(genuineMessage)
+                );
+
+                Console.WriteLine("Encrypted message is (UTF32 view): {0}",
+                                    Encoding.UTF32.GetString(genuineMessage)
+                );
+
+                Console.ReadKey();
+            }
+        }
+
+        static string PrintByteArray(byte[] encrypted_message)
+        {
+            var build_string = new StringBuilder("new byte[] { ");
+            foreach (var each_byte in encrypted_message)
+            {
+                build_string.Append(each_byte + " ");
+            }
+
+            build_string.Append("}");
+            return build_string.ToString();
+        }
+
+        static byte[] EncryptStringToBytes_Aes(string genuineText, byte[] crypto_key, byte[] initialization_vector)
+        {
+            if (string.IsNullOrEmpty(genuineText))
+                throw new ArgumentNullException("genuineText");
+
+            if (crypto_key == null || crypto_key.Length <= 0)
+                throw new ArgumentNullException("crypt_key");
+
+            if (initialization_vector == null || initialization_vector.Length <= 0)
+                throw new ArgumentNullException("initialization_vector IV");
+
+            byte[] encryption_representation;
+            using (Aes aes_algorithm = Aes.Create())
+            {
+                aes_algorithm.Key = crypto_key;
+                aes_algorithm.IV = initialization_vector;
+
+                ICryptoTransform crypto_transformation = aes_algorithm.CreateEncryptor(aes_algorithm.Key, aes_algorithm.IV);
+                using (MemoryStream memoryStreamForEncryption = new MemoryStream)
+                {
+                    using (CryptoStream cryptoStreamEncryption = new CryptoStream(memoryStreamForEncryption, crypto_transformation, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriterForEncryption = new StreamWriter(cryptoStreamEncryption))
+                        {
+                            streamWriterForEncryption.Write(genuineText);
+                        }
+                        encryption_representation = memoryStreamForEncryption.ToArray();
+                    }
+                }
+            }
+            return encryption_representation;
+        }
+
+        static string DecryptStringToBytes_Aes(byte[] encryptedText, byte[] encryption_key, byte[] initialization_vector)
+        {
+            if (encryptedText == null || encryptedText.Length <= 0)
+                throw new ArgumentNullException("encryptedText");
+
+            if (encryption_key == null || encryption_key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            
+            if (initialization_vector == null || initialization_vector.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            string original_text = null;
+
+            using(Aes aes_algorithm = Aes.Create())
+            {
+                aes_algorithm.Key = encryption_key;
+                aes_algorithm.IV = initialization_vector;
+
+                ICryptoTransform decrypt_transform = aes_algorithm.CreateDecryptor(aes_algorithm.Key, aes_algorithm.IV);
+
+                using (MemoryStream memoryStreamDecryption = new MemoryStream(encryptedText))
+                {
+                    using(CryptoStream cryptoStreamDecryption = new CryptoStream(memoryStreamDecryption, decrypt_transform, CryptoStreamMode.Read))
+                    {
+                        using(StreamReader streamReaderDecryption = new StreamReader(cryptoStreamDecryption))
+                        {
+                            original_text = streamReaderDecryption.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return original_text;
+        }
+    }
+}
